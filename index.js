c45c17165bc26ec92d32a85cbfdf8509bb336577
Pyae Phyo Maung <pyaephyomg.ppm06@gmail.com>
2025-06-17 22:33:36 +0700

    MongoDB setup

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-import express from 'express'
 
+import mongoose from 'mongoose';
 const app = express()
 const PORT = 3000
 
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+
+
 app.get("/", (req,res)=>{
-    res.send("This is from Node JS")
+    res.send("This is from Node JS but with Nodemon")
 })
 
 app.listen(PORT, ()=> {
-    console.log("Hello World")
+    console.log("Hello Ladies")
 })
+
+// pyaephyo
+// 8yvy92oOtU9zp2fO
